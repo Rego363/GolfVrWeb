fee155472e2db0b83458e61c41488520fca57c44
sean regan <rego363@gmail.com>
2018-06-18 16:22:22 +0100

    Fixed edit screen bugs

--- public/js/edit.js
+++ public/js/edit.js
@@ -4,11 +4,31 @@ function hide()
   document.getElementById("holes_div").style.display = "none";
   document.getElementById("section_div").style.display = "none";
   document.getElementById("upload_div").style.display = "none";
+  localStorage.setItem('selectCourseName', JSON.stringify("default"));
+  localStorage.setItem('selectHoleNumber', JSON.stringify("default"));
 
 }
+function hideSection()
+{
+  document.getElementById("section_div").style.display = "none";
+  document.getElementById("upload_div").style.display = "none";
+  document.getElementById("section").value = "default";
+  document.getElementById("9holes").value = "default";
+  document.getElementById("18holes").value = "default";
+}
+
 function getEmail()
 {
   return JSON.parse(localStorage.getItem('user'));
+}
+function getCourseName()
+{
+  return JSON.parse(localStorage.getItem('selectCourseName'));
+}
+function getHoleNumber()
+{
+  document.getElementById("9holes").value = "1";
+  document.getElementById("18holes").value = "1";
 }
 function getEmailDataBase()
 {
@@ -18,7 +38,10 @@ function getEmailDataBase()
 
 function revealArea()
 {
+  document.getElementById("section_div").value = "default";
   document.getElementById("section_div").style.display = "block";
+
+  //var selected = document.getElementById("selectCourse").value = "block";
 }
 function revealUpload()
 {
@@ -46,4 +69,15 @@ function setHeader()
 function setEditHeader(e)
 {
   document.getElementById("course_head").innerHTML = "Edit " + e;
+}
+
+function storeHoleName()
+{
+  var selectHoleName = document.getElementById("selectCourse").value;
+  localStorage.setItem('selectCourseName', JSON.stringify(selectHoleName));
+}
+
+function storeHoleNumber()
+{
+  localStorage.setItem('selectHoleNumber', JSON.stringify("1"));
 }
